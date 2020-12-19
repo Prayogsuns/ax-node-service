17e1c63bee24ed41a3fac2f2af9b5f4a76aecbdc
prayogsuns <prayogsuns@gmail.com>
2020-12-19 18:09:14 +0530

    node-service - v0.0.1 - DATE:20201219-0609PM

---
+++ tf/helm-terraform-node-service/custom-node-service-no-db-2/custom-node-service-no-db/main.tf
@@ -0,0 +1,84 @@
+provider "helm" {
+  version = "1.3.2"
+}
+
+locals {
+  envVars           = jsonencode(var.env-vars)
+  healthCheckConfig = jsonencode(var.health-check-config)
+  ingressAnnotations = jsonencode(var.ingress-annotations)
+  ingressRules = jsonencode(var.ingress-rules)
+}
+
+resource "helm_release" "k8s-node-service" {
+  count      = var.enabled == "true" ? 1 : 0
+
+  name  = var.svc-name
+  chart = "${path.module}/artifacts/node-service"
+  namespace = var.namespace
+  max_history = var.max-history
+
+  set {
+    name  = "enabled"
+    value = var.enabled
+  }
+
+  set {
+    name  = "replicaCount"
+    value = var.replicas
+	type = "string"
+  }
+
+  set {
+    name  = "image.repository"
+    value = var.container-image
+	type = "string"
+  }
+
+  set {
+    name  = "image.tag"
+    value = var.svc-version
+	type = "string"
+  }
+
+  set {
+    name  = "service.name"
+    value = var.svc-name
+	type = "string"
+  }
+
+  set {
+    name  = "service.port"
+    value = var.svc-port
+	type = "string"
+  }
+
+  set {
+    name  = "service.type"
+    value = var.svc-type
+    type = "string"
+  }
+
+  values = [<<EOF
+envVars: ${local.envVars}
+healthCheckConfig: ${local.healthCheckConfig}
+ingress:
+  annotations: ${local.ingressAnnotations}
+  rules: ${local.ingressRules}
+EOF
+  ]
+}
+
+resource "null_resource" "reset-pods" {
+  count      = var.enabled == "true" ? 1 : 0
+  depends_on = [helm_release.k8s-node-service]
+
+  triggers = {
+    env-var-names  = join(",", keys(var.env-vars))
+    env-var-values = join(",", values(var.env-vars))
+  }
+
+  provisioner "local-exec" {
+    command = "for p in $(kubectl get po | awk '/^${var.svc-name}/ {print $1}'); do kubectl delete po $p; done"
+  }
+}
+
---
+++ tf/helm-terraform-node-service/custom-node-service-no-db-2/custom-node-service-no-db/variables.tf
@@ -0,0 +1,61 @@
+variable "enabled" {
+  type    = string
+  default = "true"
+}
+
+variable "namespace" {
+  type    = string
+  default = "default"
+}
+
+variable "max-history" {
+  type    = string
+  default = "3"
+}
+
+variable "svc-name" {
+  type = string
+}
+
+variable "svc-port" {
+  type = string
+}
+
+variable "svc-type" {
+  type = string
+  default = "ClusterIP"
+}
+
+variable "replicas" {
+  type    = string
+  default = "3"
+}
+
+variable "container-image" {
+  type = string
+}
+
+variable "svc-version" {
+  type    = string
+  default = "latest"
+}
+
+variable "env-vars" {
+  type = map
+}
+
+variable "health-check-config" {
+  type = any
+
+  default = {}
+}
+
+variable "ingress-annotations" {
+  type = map
+  default = {}
+}
+
+variable "ingress-rules" {
+  type = list
+}
+
